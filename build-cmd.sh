9806e5d737d380e653da691f044e703a64ee633b
Rye <rye@lindenlab.com>
2024-11-14 01:46:45 -0500

    Introduce macos universal lib support

--- build-cmd.sh
+++ build-cmd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
-artifact_id="$1"
+#artifact_id="$1"
 
 # turn on verbose debugging output for logs.
 exec 4>&1; export BASH_XTRACEFD=4; set -x
@@ -17,16 +17,12 @@ set -u
 # this command to stop it doing that...
 [[ "$OSTYPE" == "cygwin" ]] && set -o igncr
 
-top="$(pwd)"
-stage="${top}"/stage
-
 case "$AUTOBUILD_PLATFORM" in
-    windows64)
+    windows*)
         autobuild="$(cygpath -u "$AUTOBUILD")"
         build_type="windows_x86_64"
     ;;
-    darwin64)
-        build_type="macos_x86_64"
+    darwin*)
         autobuild="$AUTOBUILD"
     ;;
     linux*)
@@ -39,6 +35,9 @@ case "$AUTOBUILD_PLATFORM" in
     ;;
 esac
 
+top="$(pwd)"
+stage="${top}"/stage
+
 source_environment_tempfile="$stage/source_environment.sh"
 "$autobuild" source_environment > "$source_environment_tempfile"
 . "$source_environment_tempfile"
@@ -46,10 +45,10 @@ source_environment_tempfile="$stage/source_environment.sh"
 pushd "$stage"
 
 # download the artifact
-curl -o webrtc.tar.bz2.zip -L -H "Accept: application/vnd.github+json" \
-                              -H "X-GitHub-Api-Version: 2022-11-28" \
-                              -H "Authorization: Bearer $AUTOBUILD_GITHUB_TOKEN" \
-                              https://api.github.com/repos/secondlife/3p-webrtc-build/actions/artifacts/"$artifact_id"/zip
+# curl -o webrtc.tar.bz2.zip -L -H "Accept: application/vnd.github+json" \
+#                               -H "X-GitHub-Api-Version: 2022-11-28" \
+#                               -H "Authorization: Bearer $AUTOBUILD_GITHUB_TOKEN" \
+#                               https://api.github.com/repos/secondlife/3p-webrtc-build/actions/artifacts/"$artifact_id"/zip
 
 # delete it so autobuild upload doesn't assume it's a bad autobuild artifact
 # TODO: Find a better way to do this that doesn't involve a 3rd party action
@@ -58,25 +57,59 @@ curl -o webrtc.tar.bz2.zip -L -H "Accept: application/vnd.github+json" \
 #               -H "Authorization: Bearer $AUTOBUILD_GITHUB_TOKEN" \
 #               https://api.github.com/repos/secondlife/3p-webrtc-build/actions/artifacts/"$artifact_id"
 
-unzip webrtc.tar.bz2.zip
-tar xjf webrtc.tar.bz2 --strip-components=1
-rm webrtc.tar.bz2.zip
-rm webrtc.tar.bz2
-
-# Munge the WebRTC Build package contents into something compatible
-# with the layout we use for other autobuild pacakges
-mv include webrtc
-mkdir include
-mv webrtc include
-mv lib release
-mkdir lib
-mv release lib
-mkdir LICENSES
-mv NOTICE LICENSES/webrtc-license.txt
+# unzip webrtc.tar.bz2.zip
+# tar xjf webrtc.tar.bz2 --strip-components=1
+# rm webrtc.tar.bz2.zip
+# rm webrtc.tar.bz2
 
 case "$AUTOBUILD_PLATFORM" in
-    darwin64)
-        mv Frameworks/WebRTC.xcframework/macos-x86_64/WebRTC.framework lib/release
+    darwin*)
+        mkdir "x86_64"
+        pushd "x86_64"
+            mv "$stage"/_package/webrtc.macos_x86_64.tar.bz2 webrtc.tar.bz2
+            tar -xjf webrtc.tar.bz2 --strip-components=1
+            rm webrtc.tar.bz2
+        popd
+
+        mkdir "arm64"
+        pushd "arm64"
+            mv "$stage"/_package/webrtc.macos_arm64.tar.bz2 webrtc.tar.bz2
+            tar -xjf webrtc.tar.bz2 --strip-components=1
+            rm webrtc.tar.bz2
+        popd
+
+        # Munge the WebRTC Build package contents into something compatible
+        # with the layout we use for other autobuild pacakges
+
+        # Create universal library
+        mkdir -p lib/release
+        lipo -create -output "$stage/lib/release/libwebrtc.a" "$stage/x86_64/lib/libwebrtc.a" "$stage/arm64/lib/libwebrtc.a"
+
+        mkdir -p include
+        mv x86_64/include/* include
+
+        mkdir LICENSES
+        mv x86_64/NOTICE LICENSES/webrtc-license.txt
+    ;;
+    *)
+        mv "$stage"/_package/webrtc."$build_type".tar.bz2 webrtc.tar.bz2
+        tar -xjf webrtc.tar.bz2 --strip-components=1
+        rm webrtc.tar.bz2
+        
+        # Munge the WebRTC Build package contents into something compatible
+        # with the layout we use for other autobuild pacakges
+        mv include webrtc
+        mkdir include
+        mv webrtc include
+        mv lib release
+        mkdir lib
+        mv release lib
+        mkdir LICENSES
+        mv NOTICE LICENSES/webrtc-license.txt
+    ;;
+    *)
+        echo "This project is not currently supported for $AUTOBUILD_PLATFORM" 1>&2 ; exit 1
+        
     ;;
 esac
 
